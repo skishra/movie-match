0f2edc39826b2a48561e8b0e506448d7848f4b44
YannieChiem <s0409425@salemstate.edu>
2025-03-12 13:07:20 -0400

    3/12 1:07PM Delete test failed

--- backend/server/routes/favoriteMovieDeleteMovieById.js
+++ backend/server/routes/favoriteMovieDeleteMovieById.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const newMovieModel = require("../models/favoriteMoviesModel");
+const newFavoriteMovieModel = require("../models/favoriteMoviesModel");
 
-router.delete("/favoriteMovies/deleteById/:id", async (req, res) => {
-  const { id: movieId } = req.params; // Retrieve movieId from URL params
+router.delete(
+  "/favoriteMovies/deleteFavoriteMoviesById/:id",
+  async (req, res) => {
+    const { id: movieId } = req.params; // Retrieve movieId from URL params
 
-  try {
-    // Attempt to delete the favorite movie
-    const deletedMovie = await favoriteMoviesModel.findByIdAndDelete(movieId);
+    try {
+      // Attempt to delete the favorite movie
+      const deletedMovie = await newFavoriteMovieModel.findByIdAndDelete(
+        movieId
+      );
 
-    if (!deletedMovie) {
-      return res.status(404).json({ message: "Favorite movie not found" });
+      if (!deletedMovie) {
+        return res.status(404).json({ message: "Favorite movie not found" });
+      }
+
+      return res.json({
+        message: "Favorite movie deleted successfully",
+        deletedMovie,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
     }
-
-    return res.json({
-      message: "Favorite movie deleted successfully",
-      deletedMovie,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
   }
-});
+);
 
 module.exports = router;
